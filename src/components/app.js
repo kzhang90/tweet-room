3aac8e7d8d76393561533a24b889f50408a37646
Kenny Zhang <Kenny.y.zhang@gmail.com>
2016-08-03 12:28:17 -0700

    few app.js addins

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,27 @@
 import React from 'react';
 
-export default class App extends React.Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    }
+  }
   render() {
     return (
       <div>
-        <h1>React Twitter App</h1>
+        <h1>Twitta Twatta</h1>
       </div>
     );
   }
 }
+
+App.propTypes = {
+
+}
+
+App.defaultProps = {
+
+}
+
+export default App;
