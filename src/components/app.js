59480b697402a191b88b0493b597af33cd0e3579
Kenny Zhang <Kenny.y.zhang@gmail.com>
2016-08-10 21:35:04 -0700

    adding blank lifecycle methods

--- src/components/app.js
+++ src/components/app.js
@@ -2,12 +2,37 @@ import React from 'react';
 import Feed from './feed';
 
 export default class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  componentWillMount() {
+    // called the first time the component is loaded right before the component is added to the page
+  }
+
+  componentDidMount() {
+    // called after the component has been rendered into the page
+  }
+
+  componentWillReceiveProps(nextProps) {
+    // called when the props provided to the component are changed
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    // called when the props and/or state change
+  }
+
+  componentWillUnmount() {
+    // called when the component is removed
+  }
+
   render() {
-    console.log('in the app!')
-    console.log(process.env.TWITTER_KEY);
+    console.log('in the app!');
     return (
         <div>
-          <h1>Twitta Twatta</h1>
+          <h1>tweet room</h1>
           <Feed />
         </div>
     );
