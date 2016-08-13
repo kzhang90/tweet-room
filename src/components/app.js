a9f262842f7be0141b24e3bc3b83c27115256f1f
Kenny Zhang <Kenny.y.zhang@gmail.com>
2016-08-12 23:01:43 -0700

    app component set state

--- src/components/app.js
+++ src/components/app.js
@@ -4,37 +4,42 @@ import Feed from './feed';
 export default class App extends React.Component {
 
   constructor() {
+    console.log("in the constructor");
     super();
     this.state = {
       count: 0
-      // count: props.initialCount
     };
-    // any method that is in the class needs to be bound to the instance.
     this.increment = this.increment.bind(this);
   }
 
   increment() {
-
+    this.setState({
+      count: this.state.count + 1
+    });
   }
-
+// a component cannot change its props but it can change its state
   componentWillMount() {
     // called the first time the component is loaded right before the component is added to the page
     console.log("component will mount");
   }
 
   componentDidMount() {
+    console.log("component did mount");
     // called after the component has been rendered into the page
   }
 
   componentWillReceiveProps(nextProps) {
+    console.log("component will receive props");
     // called when the props provided to the component are changed
   }
 
   componentWillUpdate(nextProps, nextState) {
+    console.log("component will update");
     // called when the props and/or state change
   }
 
   componentWillUnmount() {
+    console.log("component will unmount");
     // called when the component is removed
   }
 
@@ -50,6 +55,10 @@ export default class App extends React.Component {
   }
 }
 
-App.propTypes = {};
-App.defaultProps = {}
+App.propTypes = {
+  initialCount: React.PropTypes.number
+};
+App.defaultProps = {
+  // default props go here
+}
 
--- src/components/feed.js
+++ src/components/feed.js
@@ -3,6 +3,7 @@ import React from 'react';
 export default class Feed extends React.Component {
   render() {
     return (
+      // adjacent jsx elements must be wrapped in an enclosing jsx tag
       <div>This is the Feed</div>
     );
   }
