07e04c8e5ed22f15c76626c8fc321f6f1f3dad47
Kenny Zhang <Kenny.y.zhang@gmail.com>
2016-11-01 14:10:06 -0700

    adding twitter api

--- src/server.js
+++ src/server.js
@@ -6,10 +6,19 @@ const webpackHotMiddleware = require('webpack-hot-middleware');
 const webpackConfig = require('../webpack.config');
 const app = express();
 const compiler = webpack(webpackConfig);
+const request = require('request');
+
+require('dotenv').config();
 
 app.use(webpackDevMiddleware(compiler));
 app.use(webpackHotMiddleware(compiler));
 app.use(express.static('static'));
+
+app.get('/trending', function(req, res) {
+  const url = 'https://api.twitter.com/1.1/trends/place.json';
+  
+
+});
 
 app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'static/views/index.html'));
