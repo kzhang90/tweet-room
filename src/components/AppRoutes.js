96d753edfb13647ab9ab7ff64a0158d98e2868ab
Kenny Zhang <Kenny.y.zhang@gmail.com>
2016-11-25 12:32:45 -0800

    no more react createelement null error

--- src/components/AppRoutes.js
+++ src/components/AppRoutes.js
@@ -5,16 +5,20 @@ import { Provider } from 'react-redux';
 import { render } from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-
 // thunk allows us to dispatch async
 const store = createStore(
   (state = {}) => state,
   applyMiddleware(thunk)
 );
 
-render(
-  <Provider store={store}>
-    <Router history={browserHistory} routes={routes} onUpdate={() => window.scrollTo(0, 0)} />
-  </Provider>, 
-  document.getElementById('app')
-);
+class AppRoutes extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router history={browserHistory} routes={routes} onUpdate={() => window.scrollTo(0, 0)} />
+      </Provider>
+    );
+  }
+}
+
+export default AppRoutes;
