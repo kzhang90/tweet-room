9e6e033b9fd4b221f6b6b00b4092db8a106bcd76
Kenny Zhang <Kenny.y.zhang@gmail.com>
2016-12-12 23:11:25 -0800

    working on twitter signin

--- src/client/components/LoginForm.js
+++ src/client/components/LoginForm.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import { signIn } from '../helpers/auth';
 
 class LoginForm extends React.Component {
+  handleTwitterSubmit(e) {
+    e.preventDefault();
+    signIn();
+  }
   render() {
     return (
       <div className="loginForm">
--- src/client/helpers/auth.js
+++ src/client/helpers/auth.js
@@ -1,6 +1,34 @@
+import firebase from 'firebase';
 import { firebaseAuth } from '../config/constants';
+// firebase.auth
 
-const provider = 
+const provider = new firebaseAuth.TwitterAuthProvider();
 
+export function signIn() {
+  return firebase.auth().signInWithRedirect(provider);
+}
 
+export function retrieveTwitterToken() {
+  return firebase.auth().getRedirectResult().then(function(result) {
+          if (result.credential) {
+            var token = result.credential.accessToken;
+            var secret = result.credential.secret;
+          }
+          var user = result.user;  
+        }).catch(function(error) {
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          var email = error.email;
+          var credential = error.credential;
+        })
+  
+}
+
+export function logout() {
+  return firebase.auth().signOut().then(function() {
+   console.log("signout successful"); 
+  }, function(error) {
+    console.log(error);
+  });
+}
 
