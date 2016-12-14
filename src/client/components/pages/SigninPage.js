bc1ab97dbfdd9fa17f1d18cb66513ec7536d3850
Kenny Zhang <Kenny.y.zhang@gmail.com>
2016-12-14 12:59:14 -0800

    change login to signin page

--- src/client/components/pages/SigninPage.js
+++ src/client/components/pages/SigninPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import LoginForm from '../LoginForm';
+import { signIn } from '../helpers/auth';
 
 class Login extends React.Component {
   render() {
@@ -7,6 +8,7 @@ class Login extends React.Component {
       <div className="loginPage">
         <h1>Login Page</h1>
         <LoginForm />
+        <button>Sign In with Twitter</button>
       </div>
     );
   }
