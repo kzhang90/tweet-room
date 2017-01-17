7bcec4172a4fc4d0e86b965ebe2a0849ad02779b
Kenny Zhang <Kenny.y.zhang@gmail.com>
2017-01-17 12:22:33 -0800

    change indexpage

--- src/client/routes.js
+++ src/client/routes.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import Layout from './components/Layout';
-import IndexPage from './components/pages/IndexPage';
+import GreetingPage from './components/pages/GreetingPage';
+import HomePage from './components/pages/HomePage';
 import TagPage from './components/pages/TagPage';
 import SignupPage from './components/pages/SignupPage';
 import LoginPage from './components/pages/LoginPage';
@@ -10,10 +11,11 @@ import NotFoundPage from './components/pages/NotFoundPage';
 // a path to see all of the details of a tag
 const routes = (
     <Route path="/" component={Layout}>
-      <IndexRoute component={IndexPage} />
-      <Route path="tag/:id" component={TagPage} />
+      <IndexRoute component={GreetingPage} />
       <Route path="signup" component={SignupPage} />
       <Route path="login" component={LoginPage} />
+      <Route path="home" component={HomePage} />
+      <Route path="tag/:id" component={TagPage} />
       <Route path="*" component={NotFoundPage} />
     </Route>
 );
